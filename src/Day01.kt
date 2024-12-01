bf62084db53d1231a5b8becb6aad84d1be2c472f
Folamomo <noga.igor@gmail.com>
2024-12-01 17:44:15 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,27 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (list1, list2) = input.toIntPairs().unzip()
+        return list1.sorted()
+            .zip(list2.sorted())
+            .sumOf { (x, y) -> abs(x - y) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (list1, list2) = input.toIntPairs().unzip()
+        val count1 = list1.groupingBy { it }.eachCount()
+        val count2 = list2.groupingBy { it }.eachCount()
+        return count1.asSequence().sumOf { (k, v) -> k * v * count2.getOrDefault(k, 0) }
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+//    check(part1(listOf("test_input")) == 11)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,15 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+val whitespaceRegex = "\\s+".toRegex()
+
+fun List<String>.toIntPairs() = this.map{it.split(whitespaceRegex)}.map { Pair(it[0].toInt(), it[1].toInt()) }
+
+fun <T1, T2> List<Pair<T1, T2>>.forEachElement(action1: (T1) -> Unit, action2: (T2) -> Unit) {
+    this.forEach{
+        action1.invoke(it.first)
+        action2.invoke(it.second)
+    }
+}
+
